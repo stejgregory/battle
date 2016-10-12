9496e1da171823df213ca3da61398623472ff318
B-raw <michaelbarry1989@hotmail.com>
2016-10-12 14:09:46 +0100

    Add health points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,21 +1,16 @@
 require 'spec_helper'
+require_relative 'web_helpers'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_one_name, with: 'Ned Stark'
-    fill_in :player_two_name, with: 'Joffrey Baratheon'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ned Stark vs. Joffrey Baratheon'
   end
 end
 
 feature 'Hit Points' do
   scenario 'viewing hit points' do
-    visit('/')
-    fill_in :player_one_name, with: 'Ned Stark'
-    fill_in :player_two_name, with: 'Joffrey Baratheon'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Joffrey Baratheon HP: 60'
   end
 end
