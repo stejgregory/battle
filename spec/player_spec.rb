d70fd84f9a78b6c3a10c051e6bb498f72fc5ce52
Stephen Gregory <stejgregory@gmail.com>
2016-10-12 15:24:48 +0100

    Added RSPEC test for reducing hit points

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,10 +3,16 @@ require 'player'
 
 describe Player do
   subject { described_class.new("Sally") }
-
+  let(:player_2) { described_class.new("Claire") }
   describe 'name' do
     it "should have a name" do
       expect(subject.name).to eq "Sally"
     end
   end
+
+  describe 'attack'
+    it "should reduce hit points if you click attack" do
+      expect{subject.attack}.to change{player_2.hitpoints}.by(-10)
+    end
+
 end
