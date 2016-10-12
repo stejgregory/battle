29255fb2bce5f7fcdc45ace951e567225b9bdef8
B-raw <michaelbarry1989@hotmail.com>
2016-10-12 12:15:27 +0100

    Add failing test for HP

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,3 +9,13 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
+
+feature 'Hit Points' do
+  scenario 'viewing hit points' do
+    visit('/')
+    fill_in :player_one_name, with: 'Dave'
+    fill_in :player_two_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens HP: 60'
+  end
+end
