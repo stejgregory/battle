86b76d53b86288035d592b9a7a0777e0a29bc582
B-raw <michaelbarry1989@hotmail.com>
2016-10-12 12:20:34 +0100

    Add GoT names

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_one_name, with: 'Dave'
-    fill_in :player_two_name, with: 'Mittens'
+    fill_in :player_one_name, with: 'Ned Stark'
+    fill_in :player_two_name, with: 'Joffrey Baratheon'
     click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
+    expect(page).to have_content 'Ned Stark vs. Joffrey Baratheon'
   end
 end
 
 feature 'Hit Points' do
   scenario 'viewing hit points' do
     visit('/')
-    fill_in :player_one_name, with: 'Dave'
-    fill_in :player_two_name, with: 'Mittens'
+    fill_in :player_one_name, with: 'Ned Stark'
+    fill_in :player_two_name, with: 'Joffrey Baratheon'
     click_button 'Submit'
-    expect(page).to have_content 'Mittens HP: 60'
+    expect(page).to have_content 'Joffrey Baratheon HP: 60'
   end
 end
