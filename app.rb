538cbd11ddbddd55887ed27d8e05c1c3faa2da64
Stephen Gregory <stejgregory@gmail.com>
2016-10-12 15:43:26 +0100

    Added reduce health and atack methods to Player class

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -17,12 +17,14 @@ class Battle < Sinatra::Base
   get '/play' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
+    @player_2_health = $player_2.hit_points
     erb(:play)
   end
 
   get '/attack' do
     @player_1_name = $player_1.name
     @player_2_name = $player_2.name
+    @player_2_health = $player_2.reduce_health
     erb(:attack)
   end
 
